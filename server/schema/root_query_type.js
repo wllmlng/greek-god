73c112ba3924e29c5dd5e340820a41dc16c8c731
William Leung <w.leung.93@gmail.com>
2021-06-22 21:58:03 -0700

    complete rootquery

--- server/schema/root_query_type.js
+++ server/schema/root_query_type.js
@@ -7,6 +7,12 @@ const { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLNonNull } = graphql;
 const GodType = require("./god_type");
 const God = mongoose.model("god");
 
+const EmblemType = require("./emblem_type");
+const Emblem = mongoose.model("emblem");
+
+const AbodeType = require("./abode_type");
+const Abode = mongoose.model("abode");
+
 const RootQuery = new GraphQLObjectType({
   // remember to name your query properly
   name: "RootQueryType",
@@ -27,6 +33,32 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: new GraphQLNonNull(GraphQLID) } },
       resolve(parentValue, { id }) {
         return God.findById(id);
+      }
+    },
+    emblems: {
+      type: new GraphQLList(EmblemType),
+      resolve() {
+        return Emblem.find({});
+      }
+    },
+    emblem: {
+      type: EmblemType,
+      args: { id: {type: new GraphQLNonNull(GraphQLID) }},
+      resolve(parentValue, {id}){
+        return Emblem.findById(id);
+      }
+    },
+    abodes:{
+      type: new GraphQLList(AbodeType),
+      resolve(){
+        return Abode.find({});
+      }
+    },
+    abode:{
+      type: AbodeType,
+      args: { id: {type: new GraphQLNonNull(GraphQLID)}},
+      resolve(parentValue, {id}){
+        return Abode.findById(id);
       }
     }
   })
